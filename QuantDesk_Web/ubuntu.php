166e2d3800cbaf9902999bcfcabd5c1b6565d569
atramos@gmail.com <atramos@gmail.com@bdc4a35c-c09f-b36f-3400-4c391d039da1>
2010-05-30 18:03:02 +0000

    project reorg

---
+++ QuantDesk_Web/ubuntu.php
@@ -0,0 +1,13 @@
+<?
+
+$_CONFIG['ftp_username'] = 'atramos';
+$_CONFIG['ftp_password'] = 'One72f61tO5q';
+
+$_CONFIG['MySQLHost']                =    'localhost';
+$_CONFIG['MySQLUser']                =    'root';
+$_CONFIG['MySQLPassword']            =    'oo';
+$_CONFIG['MySQLDatabase']            =    'stock';
+
+$_CONFIG['ErrorMailAddress']        =    'admin@winance.com';
+
+?>
